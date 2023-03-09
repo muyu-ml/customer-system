022c99506940c5f867a1791e04ed1e5113ec6aca
conglongli <liconglong@163.com>
2023-03-09 14:24:07 +0800

    引入 xxl-job 定时拉取外包客服人员数据

---
+++ customer-system/src/main/java/com/lcl/galaxy/cs/xxljob/CustomerStaffSyncJobHandler.java
@@ -0,0 +1,28 @@
+package com.lcl.galaxy.cs.xxljob;
+
+import com.lcl.galaxy.cs.service.ICustomerStaffService;
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CustomerStaffSyncJobHandler {
+
+    @Autowired
+    private ICustomerStaffService customerStaffService;
+
+    @XxlJob("syncCustomerStaffJob")
+    public ReturnT<String> syncCustomerStaff() throws Exception{
+        log.info("{} ====== syncCustomerStaffJob ====== 同步客服信息");
+        // 使用钩子函数获取参数
+        String param = XxlJobHelper.getJobParam();
+        Long systemId = Long.parseLong(param);
+        // 远程调用，获取客服信息并保存
+        customerStaffService.syncGetOutsourcingCustomerStaffBySystemId(systemId);
+        return ReturnT.SUCCESS;
+    }
+}
---
+++ customer-system/src/main/java/com/lcl/galaxy/cs/xxljob/Demo2Handler.java
@@ -0,0 +1,21 @@
+package com.lcl.galaxy.cs.xxljob;
+
+import com.xxl.job.core.biz.model.ReturnT;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class Demo2Handler  {
+
+    @XxlJob("demoHandler")
+    public ReturnT<String> execute(String s) throws Exception {
+        try {
+            log.info("========demo");
+            return ReturnT.SUCCESS;
+        } catch (Exception e) {
+            return ReturnT.FAIL;
+        }
+    }
+}
