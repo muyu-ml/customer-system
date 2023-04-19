acd1cc30529ec7f460491fa97d21b95bd3f299b6
conglongli <liconglong@163.com>
2023-04-19 20:40:18 +0800

    es更新词库

---
+++ customer-system-distribution/distribution-search-service/distribution-search-service-provider/src/main/java/com/lcl/galaxy/search/service/provider/ikremote/ExtensionWord.java
@@ -0,0 +1,42 @@
+package com.lcl.galaxy.search.service.provider.ikremote;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "extension_word")
+public class ExtensionWord {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	private String word;
+	private Date createTime;
+	private Date updateTime;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getWord() {
+		return word;
+	}
+	public void setWord(String word) {
+		this.word = word;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+}
