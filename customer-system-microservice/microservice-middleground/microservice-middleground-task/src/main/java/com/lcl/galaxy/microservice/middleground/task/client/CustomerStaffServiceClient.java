d9ea4e7e08a883b33362898e8e22dc2233d6573b
conglongli <liconglong@163.com>
2023-07-13 16:43:51 +0800

    引入Nacos配置中心

--- customer-system-microservice/microservice-middleground/microservice-middleground-task/src/main/java/com/lcl/galaxy/microservice/middleground/task/client/CustomerStaffServiceClient.java
+++ customer-system-microservice/microservice-middleground/microservice-middleground-task/src/main/java/com/lcl/galaxy/microservice/middleground/task/client/CustomerStaffServiceClient.java
@@ -1,13 +1,11 @@
 package com.lcl.galaxy.microservice.middleground.task.client;
 
 import com.lcl.galaxy.microservice.middleground.customer.service.CustomerStaffSyncService;
-import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomerStaffServiceClient {
 
-    @DubboReference(version = "${customer.service.version}")
     private CustomerStaffSyncService customerStaffSyncService;
 
     public void syncGetOutsourcingCustomerStaffBySystemId(Long systemId){
