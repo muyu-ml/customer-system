be91f03ab9f2e6fb6b3da4b3e098fec825afb20b
conglongli <liconglong@163.com>
2023-08-04 00:04:20 +0800

    引入自定义认证组件

--- customer-system-microservice/microservice-frontend/microservice-frontend-chat-service/src/main/java/com/lcl/galaxy/microservice/frontend/chat/FronttendImChatApplication.java
+++ customer-system-microservice/microservice-frontend/microservice-frontend-chat-service/src/main/java/com/lcl/galaxy/microservice/frontend/chat/FronttendImChatApplication.java
@@ -1,9 +1,15 @@
 package com.lcl.galaxy.microservice.frontend.chat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.lcl.galaxy.microservice.frontend.chat","com.lcl.galaxy.microservice.service"})
+@MapperScan("com.lcl.galaxy.microservice.frontend.chat.mapper")
+@EnableJpaRepositories("com.lcl.galaxy.microservice.service.repository")
+@EntityScan("com.lcl.galaxy.microservice.service.domain")
 public class FronttendImChatApplication {
 
     public static void main(String[] args) {
