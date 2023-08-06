5bb036d27cfb5dcf85fb4ce78a7791e406ee674e
conglongli <liconglong@163.com>
2023-08-06 22:49:35 +0800

    OAuth2集成JWT：客户端

--- customer-system-microservice/microservice-frontend/microservice-frontend-chat-service/src/main/java/com/lcl/galaxy/microservice/frontend/chat/FronttendImChatApplication.java
+++ customer-system-microservice/microservice-frontend/microservice-frontend-chat-service/src/main/java/com/lcl/galaxy/microservice/frontend/chat/FronttendImChatApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.lcl.galaxy.microservice.frontend.chat","com.lcl.galaxy.microservice.security.service"})
+@SpringBootApplication(scanBasePackages = {"com.lcl.galaxy.microservice.frontend.chat"
+        ,"com.lcl.galaxy.microservice.security.service"
+        ,"com.lcl.galaxy.microservice.security.server.config"})
 @MapperScan("com.lcl.galaxy.microservice.frontend.chat.mapper")
 @EnableJpaRepositories("com.lcl.galaxy.microservice.security.service.repository")
 @EntityScan("com.lcl.galaxy.microservice.security.service.domain")
