4fedac95786ca865bfc9e8f82a13f369369f30db
conglongli <liconglong@163.com>
2023-08-06 22:17:19 +0800

    OAuth2集成JWT：配置JWT Token Store 通用配置jar

---
+++ customer-system-microservice/microservice-security/microservice-security-auth-common/src/main/java/com/lcl/galaxy/microservice/security/server/config/JWTTokenStoreConfig.java
@@ -0,0 +1,36 @@
+package com.lcl.galaxy.microservice.security.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class JWTTokenStoreConfig {
+
+    private final String SIGNING_KEY = "lcl_cs_123456";
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter(){
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey(SIGNING_KEY);
+        return jwtAccessTokenConverter;
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        return defaultTokenServices;
+    }
+}
