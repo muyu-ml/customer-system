25badbb410d32f38b0af9d1333c44c8d7024b6fd
conglongli <liconglong@163.com>
2023-08-27 10:33:08 +0800

    sentinel：限流

--- customer-system-microservice/microservice-frontend/microservice-frontend-chat-service/src/main/java/com/lcl/galaxy/microservice/frontend/chat/controller/ChatRecordController.java
+++ customer-system-microservice/microservice-frontend/microservice-frontend-chat-service/src/main/java/com/lcl/galaxy/microservice/frontend/chat/controller/ChatRecordController.java
@@ -1,13 +1,13 @@
 package com.lcl.galaxy.microservice.frontend.chat.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.lcl.galaxy.microservice.frontend.chat.controller.vo.AddChatReqVO;
 import com.lcl.galaxy.microservice.frontend.chat.service.IChatRecordService;
 import com.lcl.galaxy.microservice.middleground.task.infrastructure.vo.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -16,10 +16,22 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/chatRecords")
+@Slf4j
 public class ChatRecordController {
 
     @Autowired
     private IChatRecordService chatRecordService;
+
+
+    @GetMapping("/")
+    public Result<Boolean> getChats(){
+        return Result.success(true);
+    }
+
+    @GetMapping("/{id}")
+    public Result<String> getChatById(@PathVariable String id){
+        return Result.success(id);
+    }
 
     @PostMapping(value = "/")
     Result<Boolean> insertChat(@RequestBody AddChatReqVO addChatReqVO) {
@@ -29,5 +41,16 @@ public class ChatRecordController {
         chatRecordService.insertChat(addChatReqVO);
         return Result.success(true);
 
+    }
+
+    @GetMapping("/test")
+    @SentinelResource(value = "testChats", blockHandler = "handlerBlock")
+    public Result<Boolean> getChatsTest(){
+        return Result.success(true);
+    }
+
+    public Result<Boolean> handlerBlock(BlockException e){
+        log.info("被 Sentinel 限流了 。。。。。");
+        return Result.success(false);
     }
 }
